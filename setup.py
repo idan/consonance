d2238fce03932c09ce9e6df81ad8367afd13e227
Idan Gazit <idan@pixane.net>
2008-11-09 18:01:02 +0200

    Version bump, added long_description

--- setup.py
+++ setup.py
@@ -33,11 +33,27 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "consonance",
-    version = "0.1.2",
+    version = "0.1.3",
     packages = find_packages(),
     author = "Idan Gazit",
     author_email = "idan@pixane.net",
     description = "A django app for consuming public FriendFeed streams.",
+    long_description = """
+    FriendFeed_ is a fantastic service which does all of the hard work in
+    aggregating and "normalizing" (for lack of a better description) all of
+    the online activity we generate on socal sites like Flickr, YouTube, et
+    al.
+
+    *Consonance* is merely a set of django models which mirrors the structure
+    set forth in the `FriendFeed API Documentation`_ and a convenient
+    cron-callable script for fetching updates from FriendFeed. It should be
+    useful for integrating a lifestream into a django-powered site, but the
+    display and styling of the lifestream is left up to the user -- there are
+    no views or templates included.
+    
+    .. _FriendFeed: http://www.friendfeed.com
+    .. _FriendFeed API Documentation: http://code.google.com/p/friendfeed-api/wiki/ApiDocumentation
+    """
     url = "http://github.com/idangazit/consonance/",
     license = "BSD",
     classifiers = [
